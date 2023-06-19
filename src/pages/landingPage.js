d4e47bc6402943f62c949ba1a3b915552830ee54
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-06-19 23:50:26 +0700

    initial commit

---
+++ src/pages/landingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import button from 'elements/Button'
+
+export default class landingPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
