e45d9410ed8169a622b77ddf3c73c5fc9de1f070
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-06-21 04:01:07 +0700

    footer and testi

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,6 +3,8 @@ import Header from 'parts/header';
 import Hero from 'parts/hero';
 import MostPicked from 'parts/mostPicked';
 import Categories from 'parts/categories';
+import Testimoni from 'parts/testimoni';
+import Footer from 'parts/footer';
 import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
@@ -19,6 +21,8 @@ export default class LandingPage extends Component {
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}></Hero>
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked}></MostPicked>
         <Categories data={landingPage.categories}></Categories>
+        <Testimoni data={landingPage.testimonial}></Testimoni>
+        <Footer></Footer>
       </>
     )
   }
