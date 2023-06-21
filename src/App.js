ea77125fe9ed6dc6a6fae1ea0f280a364414dc96
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-06-21 12:35:30 +0700

    page detail (on progress) and input number component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from 'pages/landingPage';
+import itemDetails from 'pages/itemDetails';
 
 import 'assets/scss/style.scss';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={itemDetails}></Route>
       </Router>
     </div>
   );
